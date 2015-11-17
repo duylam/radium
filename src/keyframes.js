bd6599e739a0f033430dbc81a047211762c3b8fb
Ian Obermiller <ian@obermillers.com>
2015-11-16 23:18:00 -0800

    Update keyframes to use StyleKeeper

--- src/keyframes.js
+++ src/keyframes.js
@@ -1,75 +1,34 @@
 /* @flow */
 
-import camelCasePropsToDashCase from './camel-case-props-to-dash-case';
-import createMarkupForStyles from './create-markup-for-styles';
-import {getPrefixedStyle} from './prefixer';
-
-import ExecutionEnvironment from 'exenv';
-
-let isAnimationSupported = false;
-let keyframesPrefixed = 'keyframes';
-
-if (ExecutionEnvironment.canUseDOM) {
-  // Animation feature detection and keyframes prefixing from MDN:
-  // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Detecting_CSS_animation_support
-  const domPrefixes = ['Webkit', 'Moz', 'O', 'ms'];
-  const element = (document.createElement('div'): any);
-
-  if (element.style.animationName !== undefined) {
-    isAnimationSupported = true;
-  } else {
-    domPrefixes.some(prefix => {
-      if (element.style[prefix + 'AnimationName'] !== undefined) {
-        keyframesPrefixed = '-' + prefix.toLowerCase() + '-keyframes';
-        isAnimationSupported = true;
-        return true;
-      }
-      return false;
-    });
-  }
-}
+import cssRuleSetToString from './css-rule-set-to-string';
 
 let animationIndex = 1;
-let animationStyleSheet = null;
 
-if (isAnimationSupported) {
-  animationStyleSheet = (document.createElement('style'): any);
-  document.head.appendChild(animationStyleSheet);
-}
-
-// Simple animation helper that injects CSS into a style object containing the
-// keyframes, and returns a string with the generated animation name.
 export default function keyframes(
   keyframeRules: {[percentage: string]: {[key: string]: string|number}},
-  componentName?: string,
-  prefix: (style: Object) => Object = getPrefixedStyle
-): string {
-  const name = 'Animation' + animationIndex;
+  name?: string,
+): {
+  __radiumKeyframes: bool,
+  name: string,
+  getCSS(userAgent: string): string
+} {
+  const animationName = (name ? name + '-' : '') + 'Animation' + animationIndex;
   animationIndex += 1;
 
-  if (!isAnimationSupported) {
-    return name;
-  }
-
-  const rule = '@' + keyframesPrefixed + ' ' + name + ' {\n' +
-    Object.keys(keyframeRules).map(percentage => {
-      const props = keyframeRules[percentage];
-      const prefixedProps = prefix(props);
-      const cssPrefixedProps = camelCasePropsToDashCase(prefixedProps);
-      const serializedProps = createMarkupForStyles(cssPrefixedProps, '  ');
-      return '  ' + percentage + ' {\n  ' + serializedProps + '\n  }';
-    }).join('\n') +
-    '\n}\n';
-
-  // for flow
-  /* istanbul ignore next */
-  if (!animationStyleSheet) {
-    throw new Error('keyframes not initialized properly');
-  }
-
-  animationStyleSheet.sheet.insertRule(
-    rule,
-    animationStyleSheet.sheet.cssRules.length
-  );
-  return name;
+  return {
+    __radiumKeyframes: true,
+    name: animationName,
+    getCSS(userAgent: string) {
+      const keyframesPrefixed = '-webkit-keyframes';
+      return '@' + keyframesPrefixed + ' ' + animationName + ' {\n' +
+        Object.keys(keyframeRules).map(percentage =>
+          cssRuleSetToString(
+            percentage,
+            keyframeRules[percentage],
+            userAgent
+          )
+        ).join('\n') +
+        '\n}\n';
+    }
+  };
 }
